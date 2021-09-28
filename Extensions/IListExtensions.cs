1b8d258829ce1730bdf1d096dbd2b03fee1b8268
nowhh01 <39201456+nowhh01@users.noreply.github.com>
2021-09-27 23:28:37 -0400

    add extension method for IList

---
+++ Extensions/IListExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace SortingVisualiser.Extensions
+{
+    public static class IListExtensions
+    {
+        public static void Swap<T>(this IList<T> array, int index1, int index2)
+        {
+            T temp = array[index1];
+            array[index1] = array[index2];
+            array[index2] = temp;
+        }
+    }
+}
