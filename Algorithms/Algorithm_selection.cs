bc76c86ea706d165cfe822f9488b877fb7a68f39
nowhh01 <39201456+nowhh01@users.noreply.github.com>
2021-10-13 20:27:53 -0400

    split class

---
+++ Algorithms/Algorithm_selection.cs
@@ -0,0 +1,43 @@
+﻿using SortingVisualiser.Commands;
+using System;
+using System.Collections.Generic;
+
+namespace SortingVisualiser
+{
+    public partial class Algorithm
+    {
+        public static IEnumerator<ICommand> SelectionSort(
+            IList<int> numbers,
+            Func<int, int, ICommand> changeComparedIndices,
+            Func<int, int, ICommand> swapIndices,
+            Func<int, ICommand> markAsSortedIndex
+            )
+        {
+            int count = numbers.Count;
+
+            for (int i = 0; i < count - 1; i++)
+            {
+                int leastIndex = i;
+                int comparedIndex = i + 1;
+                for (; comparedIndex < count; comparedIndex++)
+                {
+                    yield return changeComparedIndices(leastIndex, comparedIndex);
+
+                    if (numbers[leastIndex] > numbers[comparedIndex])
+                    {
+                        leastIndex = comparedIndex;
+                    }
+                }
+
+                if (leastIndex != i)
+                {
+                    yield return swapIndices(leastIndex, i);
+                }
+
+                yield return markAsSortedIndex(i);
+            }
+
+            yield return markAsSortedIndex(count - 1);
+        }
+    }
+}
